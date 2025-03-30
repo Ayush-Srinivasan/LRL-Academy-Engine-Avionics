cede34c2e51806405e95e2860bd92c55ec67b6cc
Ayush Srinivasan <asrinivasan1@cpp.edu>
2025-03-29 17:06:37 -0700

    added functions to replace math

--- LRL_Academy/src/sensorData.cpp
+++ LRL_Academy/src/sensorData.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "sensorData.h"
 
 //PT libraries and variables
 #include <Wire.h>
@@ -7,16 +8,19 @@
 //initalizing ads 1115
 Adafruit_ADS1115 ads1; //first ads 1115 (for pts 1-4) gnd connection (address 0x48)
 Adafruit_ADS1115 ads2; //second ads 1115 (for pts 5-8) vdd connection (address 0x49)
+const int PSI = 300;
+const float ADS_Gain = 0.1875/1000;
 
 //pt port variables (0,1,2,3, are the adc ports)
-const int PT1 = 0; //AIN0 on ads1
-const int PT2 = 1; //AIN1 on ads1
-const int PT3 = 2; //AIN2 on ads1
-const int PT4 = 3; //AIN3 on ads1
-const int PT5 = 0; //AIN0 on ads1
-const int PT6 = 1; //AIN1 on ads1
-const int PT7 = 2; //AIN2 on ads1
-const int PT8 = 3; //AIN3 on ads1
+const int adc1 = 0; //AIN0 on ads1
+const int adc2 = 1; //AIN1 on ads1
+const int adc3 = 2; //AIN2 on ads1
+const int adc4 = 3; //AIN3 on ads1
+
+const int adc5 = 0; //AIN0 on ads2
+const int adc6 = 1; //AIN1 on ads2
+const int adc7 = 2; //AIN2 on ads2
+const int adc8 = 3; //AIN3 on ads2
 
 //TC libraries and variables
 #include <SPI.h>
@@ -42,20 +46,26 @@ Adafruit_MAX31855 tcThroat(TC5_CS, CLK, MISO);
 Adafruit_MAX31855 tcDiverging(TC6_CS, CLK, MISO);
 
 // put function declarations here:
+float readPressureData(int16_t adcnumber);
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   //ads initialize
   if (!ads1.begin(0x48)) {
     Serial.println("ADS1 failure");
     while(1);
   }
+  ads1.setGain(GAIN_TWOTHIRDS);
+  
   if (!ads2.begin(0x49)) {
     Serial.println("ADS2 failure");
     while(1);
   }
+  ads2.setGain(GAIN_TWOTHIRDS);
+// ads.setGain(GAIN_TWOTHIRDS);  +/- 6.144V  1 bit = 0.1875mV (default)
+
 
   //max initialize
   if (!tcInjector.begin()) {
@@ -85,5 +95,59 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // ads read data
+  int16_t PT1 = ads1.readADC_SingleEnded(adc1);
+  int16_t PT2 = ads1.readADC_SingleEnded(adc2);
+  int16_t PT3 = ads1.readADC_SingleEnded(adc3);
+  int16_t PT4 = ads1.readADC_SingleEnded(adc4);
+  int16_t PT5 = ads2.readADC_SingleEnded(adc5);
+  int16_t PT6 = ads2.readADC_SingleEnded(adc6);
+  int16_t PT7 = ads2.readADC_SingleEnded(adc7);
+  int16_t PT8 = ads2.readADC_SingleEnded(adc8);
+
+
+  //converts from voltage to psi 
+  float PT01 = readPressureData(PT1);
+  float PT02 = readPressureData(PT2);
+  float PT03 = readPressureData(PT3);
+  float PT04 = readPressureData(PT4);
+  float PT05 = readPressureData(PT5);
+  float PT06 = readPressureData(PT6);
+  float PT07 = readPressureData(PT7);
+  float PT08 = readPressureData(PT8);
+
+  // MAX31855 read data
+  float InjectorTemp = tcInjector.readCelsius();
+  float Converging1Temp = tcConverging1.readCelsius();
+  float Converging2Temp = tcConverging2.readCelsius();
+  float Converging3Temp = tcConverging3.readCelsius();
+  float ThroatTemp = tcThroat.readCelsius();
+  float DivergingTemp = tcDiverging.readCelsius();  
+
+ // print data
+  Serial.print("TC1: "); Serial.println(InjectorTemp);
+  Serial.print("TC2: "); Serial.println(Converging1Temp);
+  Serial.print("TC3: "); Serial.println(Converging2Temp);
+  Serial.print("TC4: "); Serial.println(Converging3Temp);
+  Serial.print("TC5: "); Serial.println(ThroatTemp);
+  Serial.print("TC6: "); Serial.println(DivergingTemp);
+  Serial.print("PT1: "); Serial.println(PT01);
+  Serial.print("PT2: "); Serial.println(PT02);
+  Serial.print("PT3: "); Serial.println(PT03);
+  Serial.print("PT4: "); Serial.println(PT04);
+  Serial.print("PT5: "); Serial.println(PT05);
+  Serial.print("PT6: "); Serial.println(PT06);
+  Serial.print("PT7: "); Serial.println(PT07);
+  Serial.print("PT8: "); Serial.println(PT08);
+  Serial.println("--------------------");
+
+  //delay
+  delay(500);
+}
+
+float readPressureData(int16_t adcnumber) {
+  float voltage = adcnumber * ADS_Gain; // multiplies ADS values by gain value
+  float pressure = PSI * ((voltage-0.5)/4); //converts voltage to psi
+  return pressure; //returns pressure
 }
---
+++ LRL_Academy/src/sensorData.h
@@ -0,0 +1,33 @@
+#ifndef SENSORDATA_H
+#define SENSORDATA_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
+#include <Adafruit_MAX31855.h>
+
+//constants
+const int PSI = 300;
+const float ADS_Gain = 0.1875/1000;
+    //adc pins
+extern Adafruit_ADS1115 ads1; //first ads 1115 (for pts 1-4) gnd connection (address 0x48)
+extern Adafruit_ADS1115 ads2; //second ads 1115 (for pts 5-8) vdd connection (address 0x49)
+extern const int adc1, adc2, adc3, adc4, adc5, adc6, adc7, adc8; 
+
+    //max31855 spi pins
+const int MISO = 12;
+const int CLK = 27;
+    
+    //tc pin chip select numbers
+const int TC1_CS = 0;
+const int TC2_CS = 1;
+const int TC3_CS = 2;
+const int TC4_CS = 3;
+const int TC5_CS = 4;
+const int TC6_CS = 5; 
+
+//functions
+float readPressureData(int16_t adcnumber);
+
+#endif
